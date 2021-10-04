55f2189a8208da8b1cf524861369ee3d280ceedb
Jay Patel <jaydpatel2015@gmail.com>
2021-10-04 20:19:37 +0530

    Filter by category finished

--- functions/functions.php
+++ functions/functions.php
@@ -114,48 +114,48 @@ function getProdCatAfterFilter()
 function getCatAfterFilter()
 {
     global $db;
-    if (isset($_GET['cat_id'])) {
-        $cat_id = $_GET['cat_id'];
-        $get_cat_query = "SELECT * FROM categories WHERE category_id='$cat_id'";
+    if (isset($_GET['cat'])) {
+        $cat_id = $_GET['cat'];
+        $get_cat_query = "SELECT * FROM categories WHERE cat_id='$cat_id'";
         $run_get_cat = mysqli_query($db, $get_cat_query);
         $row_cat = mysqli_fetch_array($run_get_cat);
-        $get_cat_title = $row_cat['category_title'];
-        $get_cat_desc = $row_cat['category_desc'];
-        $get_products = "SELECT * FROM products WHERE category_id='$cat_id'";
+        $get_cat_title = $row_cat['cat_title'];
+        $get_cat_desc = $row_cat['cat_desc'];
+        $get_products = "SELECT * FROM products WHERE cat_id='$cat_id'";
         $run_products = mysqli_query($db, $get_products);
-        $count_products = mysqli_num_rows($run_products);
-        if ($count_products == 0) {
-            echo "<div class='col-md-12 col-sm-6'>
-                <div class='card'>
-                    <h1 class='card-heading text-center'>No Results Found in " . $get_cat_title . " Category</h1>
-                </div>                        
-            </div>";
-        } else {
-            echo "<div class='col-md-12 col-sm-6'>
-                    <div class='card'>
-                        <h5 class='card-title text-center p-2'>$get_cat_title</h5>
-                        <p class='card-subctitle mb-2 text-muted text-left p-2'>$get_cat_desc</p>
+        $count_pro = mysqli_num_rows($run_products);
+        if ($count_pro == 0) {
+            echo "<div class='col-lg-12'>
+                    <div class='box'>
+                            <h1 class='text-center'>No Products Found in " . $get_cat_title . " Category</h1>
                     </div>                        
-            </div>";
+                </div>";
+        } else {
+            echo "<div class='col-lg-12'>
+                    <div class='box'>
+                        <h1 class='text-left'>$get_cat_title</h1>
+                        <p class='text-muted text-left'>$get_cat_desc</p>
+                    </div>                        
+                </div>";
         }
         while ($row_products = mysqli_fetch_array($run_products)) {
             $pro_id = $row_products['product_id'];
             $pro_title = $row_products['product_title'];
             $pro_price = $row_products['product_price'];
-            $pro_img = $row_products['product_img'];
+            $pro_img1 = $row_products['product_img1'];
             echo "<div class='col-md-4 col-sm-6 mt-2'>
-                  <div class='card text-center mb-1'>
-                    <div class='card-heading p-1'>
-                        <a href='details.php?product_id=$pro_id'><img class='card-img-top' src='admin_area/admin_product_images/$pro_img' alt='men's t-shirts'></a>
-                    </div>
-                    <div class='card-body'> 
-                        <h5 class='card-title'><a href='details.php?product_id=$pro_id'>$pro_title</a></h5>
-                        <p class='card-text'><strong>&#8377;</strong>$pro_price</p>
-                        <a href='details.php?prod_id=$pro_id' class='btn btn-primary mb-1'><i class='bi bi-info-circle'></i> View Details</a>
-                        <a href='details.php?prod_id=$pro_id' class='btn btn-success'><i class='bi bi-cart-plus'></i> Add to Cart</a>
-                    </div>
-                </div>
-            </div>";
+            <div class='card text-center mb-1'>
+              <div class='card-heading p-1'>
+                  <a href='details.php?product_id=$pro_id'><img class='card-img-top' src='admin_area/admin_product_images/$pro_img1' alt='men's t-shirts'></a>
+              </div>
+              <div class='card-body'> 
+                  <h4 class='card-title'><a href='details.php?product_id=$pro_id'>$pro_title</a></h4>
+                  <h4 class='card-text'><strong>&#8377;</strong>$pro_price</h4>
+                  <a href='details.php?prod_id=$pro_id' class='btn btn-primary'><i class='bi bi-info-circle'></i> View Details</a>
+                  <a href='details.php?prod_id=$pro_id' class='btn btn-primary'><i class='bi bi-cart-plus'></i> Add to Cart</a>
+              </div>
+          </div>
+      </div>";
         }
     }
 }
--- shop.php
+++ shop.php
@@ -112,25 +112,26 @@ include('functions/functions.php');
                   </div>
               <?php }
               } ?>
-              <div class="row products">
-                <?php getProdCatAfterFilter(); ?>
-                <?php if (!isset($_GET['p_cat'])) {
-                  if (!isset($_GET['cat'])) {
-                    $per_page = 3;
-                    if (isset($_GET['page'])) {
-                      $page = $_GET['page'];
-                    } else {
-                      $page = 1;
-                    }
-                    $start_from = ($page - 1) * $per_page;
-                    $get_products = "SELECT * FROM products order by 1 DESC LIMIT $start_from,$per_page";
-                    $run_product = mysqli_query($con, $get_products);
-                    while ($res_products = mysqli_fetch_array($run_product)) {
-                      $pro_id = $res_products['product_id'];
-                      $pro_title = $res_products['product_title'];
-                      $pro_price = $res_products['product_price'];
-                      $pro_img1 = $res_products['product_img1'];
-                      echo "<div class='col-lg-4 col-md-6 d-flex'>
+                <div class="row products justify-content-center" >
+                  <?php getProdCatAfterFilter();
+                        getCatAfterFilter(); ?>
+                    <?php if (!isset($_GET['p_cat'])) {
+                      if (!isset($_GET['cat'])) {
+                        $per_page = 3;
+                        if (isset($_GET['page'])) {
+                          $page = $_GET['page'];
+                        } else {
+                          $page = 1;
+                        }
+                        $start_from = ($page - 1) * $per_page;
+                        $get_products = "SELECT * FROM products order by 1 DESC LIMIT $start_from,$per_page";
+                        $run_product = mysqli_query($con, $get_products);
+                        while ($res_products = mysqli_fetch_array($run_product)) {
+                          $pro_id = $res_products['product_id'];
+                          $pro_title = $res_products['product_title'];
+                          $pro_price = $res_products['product_price'];
+                          $pro_img1 = $res_products['product_img1'];
+                          echo "<div class='col-lg-4 col-md-6 d-flex'>
                                 <div class='product'>
                                       <div class='card text-center'>
                                             <div class='card-header'>
@@ -139,54 +140,54 @@ include('functions/functions.php');
                                             <div class='card-body'>
                                                 <h4 class='card-title'><a href='details.php?pro_id=$pro_id'>$pro_title</a></h4>
                                                 <p class='card-text'>&#8377; $pro_price</p>
-                                                  <a href='details.php?pro_id=$pro_id' class='btn btn-primary btn-sm'><i class='fa fa-search'></i>View Details</a>
-                                                  <a href='details.php?pro_id=$pro_id' class='btn btn-primary btn-sm'><i class='fa fa-cart-plus'></i> Add to Cart</a>
+                                                 <a href='details.php?pro_id=$pro_id' class='btn btn-primary btn-sm'><i class='fa fa-search'></i>View Details</a>
+                                                <a href='details.php?pro_id=$pro_id' class='btn btn-primary btn-sm'><i class='fa fa-cart-plus'></i> Add to Cart</a>
                                             </div>
                                         </div>
                                 </div>
                           </div>";
-                    }
-                ?>
-                    <!-- </div> -->
-                    <!-- /.product-->
-                    <!-- </div> -->
-                    <!-- /.products-->
-                <?php }
-                } ?>
-              </div>
-              <div class="pages justify-content-center">
-                <nav aria-label="Page navigation example">
-                  <ul class="pagination">
-                    <?php
-                    $query = "SELECT * FROM products";
-                    $result = mysqli_query($con, $query);
-                    $total_records = mysqli_num_rows($result);
-                    $total_per_page = ceil($total_records / $per_page);
-                    echo "<li class='page-item'><a href='shop.php?page=1' aria-label='Previous' class='page-link'><span aria-hidden='true'>First</span><span class='sr-only'>Previous</span></a></li>";
-                    for ($i = 1; $i < $total_per_page; $i++) {
-                      echo "<li class='page-item'><a href='shop.php?page=$i' class='page-link'><span aria-hidden='true'>$i</span><span class='sr-only'></span></a></li>";
-                    }
-                    echo "<li class='page-item'><a href='shop.php?page=$total_per_page' aria-label='Last' class='page-link'><span aria-hidden='true'>Last Page</span><span class='sr-only'>Next</span></a></li>";
+                        }
                     ?>
-                  </ul>
-                </nav>
-              </div>
-              <!-- /.col-lg-9-->
+                  <!-- </div> -->
+                  <!-- /.product-->
+                  <!-- </div> -->
+                <!-- /.products-->
+                <div class="pages d-flex justify-content-center">
+                      <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <?php 
+                                  $query="SELECT * FROM products";
+                                  $result=mysqli_query($con,$query);
+                                  $total_records=mysqli_num_rows($result);
+                                  $total_per_page=ceil($total_records/$per_page);
+                                  echo "<li class='page-item'><a href='shop.php?page=1' aria-label='Previous' class='page-link'><span aria-hidden='true'>First</span><span class='sr-only'>Previous</span></a></li>";
+                                  for($i=1;$i<$total_per_page;$i++){
+                                    echo "<li class='page-item'><a href='shop.php?page=$i' class='page-link'><span aria-hidden='true'>$i</span><span class='sr-only'></span></a></li>";
+                                  }
+                                  echo "<li class='page-item'><a href='shop.php?page=$total_per_page' aria-label='Last' class='page-link'><span aria-hidden='true'>Last Page</span><span class='sr-only'>Next</span></a></li>";
+                            ?>
+                        </ul>
+                      </nav>
+                </div>
+            <?php }
+                    } ?>
             </div>
+            <!-- /.col-lg-9-->
           </div>
         </div>
       </div>
-      <!-- *** FOOTER STARTS *** -->
-      <?php include('includes/footer.php') ?>
-      <!-- *** FOOTER END ***-->
+    </div>
+    <!-- *** FOOTER STARTS *** -->
+    <?php include('includes/footer.php') ?>
+    <!-- *** FOOTER END ***-->
 
-      <!-- JavaScript files-->
-      <script src="styles/jquery/jquery.min.js"></script>
-      <script src="styles/bootstrap/js/bootstrap.bundle.min.js"></script>
-      <script src="styles/jquery.cookie/jquery.cookie.js"> </script>
-      <script src="styles/owl.carousel/owl.carousel.min.js"></script>
-      <script src="styles/owl.carousel2.thumbs/owl.carousel2.thumbs.js"></script>
-      <script src="js/front.js"></script>
+    <!-- JavaScript files-->
+    <script src="styles/jquery/jquery.min.js"></script>
+    <script src="styles/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="styles/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="styles/owl.carousel/owl.carousel.min.js"></script>
+    <script src="styles/owl.carousel2.thumbs/owl.carousel2.thumbs.js"></script>
+    <script src="js/front.js"></script>
 </body>
 
 </html>
